d9adf27f2261018f8858ef96fccda2194cd61d24
Amit Chavda <amit.c@simformsolutions.com>
2022-03-07 12:44:20 +0530

    Added return student list

--- src/main/java/com/hello/StudentController.java
+++ src/main/java/com/hello/StudentController.java
@@ -1,5 +1,8 @@
 package com.hello;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,17 @@ public class StudentController {
 	@GetMapping("GetStudent")
 	public Student getStudent() {
 		return new Student("Tony", "Stark");
+	}
+	
+	@GetMapping("GetStudents")
+	public List<Student> getStudents(){
+		List<Student> studentList = new ArrayList();
+		
+		studentList.add(new Student("Mike","Scott"));
+		studentList.add(new Student("Tony ","Stark"));
+		studentList.add(new Student("Ragnar","Lorthbrok"));
+		studentList.add(new Student("John","Cena"));
+		studentList.add(new Student("Scarlet","Wilson"));
+		return studentList;
 	}
 }
